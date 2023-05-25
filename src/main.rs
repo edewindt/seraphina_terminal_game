f7d2092397881d738b5a8a345f8041904b1bcdc2
Elias De Windt <edewindt27@outlook.com>
2023-05-25 07:26:16 -0700

    added another conditional based on the selection chosen

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
     let mut input = String::new();
     let mut i2 = String::new();
+    let selected_option;
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
@@ -13,8 +14,9 @@ fn main() {
     println!("Hello, {}!", input.trim());
     intro::greet(input);
     println!();
-    choose();
-    println!();
+    selected_option = choose();
+    game(selected_option);
+    println!("{}", selected_option);
     choose2();
     println!("Press Enter To Finish The Game");
     io::stdin()
@@ -23,7 +25,7 @@ fn main() {
     
 }
 
-fn choose() {
+fn choose() -> &'static str {
     let options = ["Easy", "Medium", "Hard", "Impossible"];
 
     let selection = Select::with_theme(&ColorfulTheme::default())
@@ -35,10 +37,21 @@ fn choose() {
     println!("Selected: {}", options[selection]);
 
     match selection {
-        0 => println!("I see, so you're a sissy, too scared to get your hands dirty? That's okay, we'll take it easy on you."),
-        1 => println!("I see, so you won't give me up all the way, but you're still too much of coward to choose anything harder than average, kind of lame"),
-        2 => println!("You think you're a tough guy huh? Why didn't you choose impossible, I guess you're still just a beta"),
-        3 => println!("I have nothing to say, the choice speaks for itself."),
+        0 => {println!("I see, so you're a sissy, too scared to get your hands dirty? That's okay, we'll take it easy on you.")},
+        1 => {println!("I see, so you won't give me up all the way, but you're still too much of coward to choose anything harder than average, kind of lame")},
+        2 => {println!("You think you're a tough guy huh? Why didn't you choose impossible, I guess you're still just a beta")},
+        3 => {println!("I have nothing to say, the choice speaks for itself.")},
+        _ => unreachable!(),
+    }
+    options[selection]
+}
+
+fn game(con:&str) {
+    match con {
+        "Easy" => println!("So freaking easy"),
+        "Medium" => println!("Meh, its medium"),
+        "Hard" => println!("Okay, its getting spicy now"),
+        "Impossible" => println!("Now we're talking WHOOOO BABY!"),
         _ => unreachable!(),
     }
 }
@@ -63,7 +76,23 @@ let selections = MultiSelect::new()
 
 // Print the selected options
 println!("Selected options:");
-for selection in selections {
-    println!("{}", options[selection]);
+for selection in &selections {
+    println!("{}", options[*selection]);
 }
+    // Perform logic based on the selected options
+    match selections.as_slice() {
+        [0, 1, 2] => {
+            println!("Logic for options 1, 2, and 3");
+            // Perform specific actions for options 1, 2, and 3
+        }
+        [3, 4] => {
+            println!("Logic for options 4 and 5");
+            // Perform specific actions for options 4 and 5
+        }
+        _ => {
+            println!("No specific logic for selected options");
+            // Perform default actions if none of the specific cases match
+        }
+    }
+
 }
